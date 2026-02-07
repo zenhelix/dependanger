47286bb49cd8b8bdd3539afe4158891b2302a007
dmitriimedakin <dm.medakin.online@gmail.com>
2026-02-07 15:07:13 +0100

    init

--- components/core/dependanger-core/src/main/kotlin/io/github/zenhelix/dependanger/core/model/ProcessingPreset.kt
+++ components/core/dependanger-core/src/main/kotlin/io/github/zenhelix/dependanger/core/model/ProcessingPreset.kt
@@ -1,5 +1,8 @@
 package io.github.zenhelix.dependanger.core.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 public enum class ProcessingPreset {
     DEFAULT, MINIMAL, STRICT, DISTRIBUTION
 }

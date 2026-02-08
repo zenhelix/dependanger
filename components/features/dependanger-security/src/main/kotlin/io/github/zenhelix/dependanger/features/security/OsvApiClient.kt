9261f1c9b20bed750c71a170f0df2553d75d5173
dmitriimedakin <dm.medakin.online@gmail.com>
2026-02-08 21:04:55 +0100

    init

--- components/features/dependanger-security/src/main/kotlin/io/github/zenhelix/dependanger/features/security/OsvApiClient.kt
+++ components/features/dependanger-security/src/main/kotlin/io/github/zenhelix/dependanger/features/security/OsvApiClient.kt
@@ -2,8 +2,14 @@ package io.github.zenhelix.dependanger.features.security
 
 import io.github.zenhelix.dependanger.features.security.model.VulnerabilityInfo
 
+public data class PackageQuery(
+    val group: String,
+    val artifact: String,
+    val version: String,
+)
+
 public class OsvApiClient(
     public val apiUrl: String = "https://api.osv.dev",
 ) {
-    public suspend fun queryVulnerabilities(group: String, artifact: String, version: String): List<VulnerabilityInfo> = TODO()
+    public suspend fun queryBatch(packages: List<PackageQuery>): List<List<VulnerabilityInfo>> = TODO()
 }
